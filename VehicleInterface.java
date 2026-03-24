1d6587b14ddba483bf32f36a8afa83ca58242513
your-tears-amidst-the-desert <110824455+your-tears-amidst-the-desert@users.noreply.github.com>
2026-03-24 21:56:12 +0400

    Add Factory Method transport creation workflow

--- VehicleInterface.java
+++ VehicleInterface.java
@@ -1,18 +1,4 @@
 package main.java;
 
-public interface VehicleInterface {
-    String getBrand();
-    void setBrand(String brand);
-
-    String[] getAllModelNames();
-    float[] getAllModelPrices();
-    float getModelPrice(String modelName) throws VehicleException, VehicleError;
-    void updateModelPrice(String modelName, float newPrice) throws VehicleException;
-
-
-    void addModel(String modelName, float modelPrice) throws VehicleException, VehicleError;
-    void updateModelName(String oldName, String newName) throws VehicleException;
-    void removeModel(String modelName) throws VehicleException;
-
-    int getModelCount();
+public interface VehicleInterface extends Transport {
 }
